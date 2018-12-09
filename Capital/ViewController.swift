9716519494b9d18043b794b304c3b456a3ce20e1
Andrey Manakov <manakov@gmail.com>
2018-12-09 13:09:54 +0300

    Initial Commit

---
+++ Capital/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Capital
+//
+//  Created by Andrey Manakov on 09/12/2018.
+//  Copyright © 2018 Andrey Manakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
