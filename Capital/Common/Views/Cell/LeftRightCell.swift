47763e77c991f162fd0191a901b2edc628c3bcc9
Andrey Manakov <manakov@gmail.com>
2019-01-10 17:50:49 +0300

    swift lint updates

--- Capital/Common/Views/Cell/LeftRightCell.swift
+++ Capital/Common/Views/Cell/LeftRightCell.swift
@@ -1,19 +1,17 @@
-import UIKit
-
-protocol LeftRightCellProtocol: class {
+internal protocol LeftRightCellProtocol: class {
     var textLabel: UILabel? { get }
     var detailTextLabel: UILabel? { get }
     var accessoryType: UITableViewCell.AccessoryType { get set }
 }
 
-class LeftRightCell: UITableViewCell, LeftRightCellProtocol {
+internal final class LeftRightCell: UITableViewCell, LeftRightCellProtocol {
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .value1, reuseIdentifier: reuseIdentifier)
         detailTextLabel?.textColor = .black
     }
 
-    required init?(coder aDecoder: NSCoder) {
+    required internal init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
