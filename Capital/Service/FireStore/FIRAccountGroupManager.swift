9377c2116e0796e4fece9d55b661b73de6330435
Andrey Manakov <manakov@gmail.com>
2019-01-11 23:51:09 +0300

    Update FIRAccountGroupManager.swift

--- Capital/Service/FireStore/FIRAccountGroupManager.swift
+++ Capital/Service/FireStore/FIRAccountGroupManager.swift
@@ -17,6 +17,29 @@ internal final class FIRAccountGroupManager: FIRManager, FIRAccountGroupManagerP
 
     override private init() {}
 
+    private func calcAmountGroupAmount(withAccounts accounts: [Account]) -> Int {
+        let positiveAmount: Int = accounts.filter {
+            $0.type == AccountType.asset
+            }.map {
+                $0.amount ?? 0
+            }.reduce(0, +)
+        let negativeAmount: Int = accounts.filter {
+            $0.type == AccountType.liability
+            }.map { $0.amount ?? 0 }.reduce(0, +)
+        return  positiveAmount - negativeAmount
+    }
+
+    private func getAccounts(
+        withIds id: [String],
+        inTransaction fsTransaction: Transaction,
+        withErrorPointer errorPointer: NSErrorPointer
+        ) -> [String: Account] {
+        return Dictionary(uniqueKeysWithValues:
+            id.map {
+                (id: $0, (self.getAccount(withId: $0, for: fsTransaction, with: errorPointer))!)
+        })
+    }
+
     /// Creates Account Group in FireStore data base, also updates accounts
     ///
     /// - Parameters:
@@ -28,49 +51,15 @@ internal final class FIRAccountGroupManager: FIRManager, FIRAccountGroupManagerP
                 return
         }
 
-        func calcAmountGroupAmount(withAccounts accounts: [Account]) -> Int {
-            let positiveAmount: Int = accounts.filter {
-                $0.type == AccountType.asset
-                }.map {
-                    $0.amount ?? 0
-                }.reduce(0, +)
-            let negativeAmount: Int = accounts.filter {
-                $0.type == AccountType.liability
-                }.map { $0.amount ?? 0 }.reduce(0, +)
-            return  positiveAmount - negativeAmount
-        }
-
-        func getAccounts(
-            withIds id: [String],
-            inTransaction fsTransaction: Transaction,
-            withErrorPointer errorPointer: NSErrorPointer
-            ) -> [String: Account] {
-            return Dictionary(uniqueKeysWithValues:
-                accountIds.map {
-                    (id: $0, (self.getAccount(withId: $0, for: fsTransaction, with: errorPointer))!)
-            })
-        }
-
         fireDB.runTransaction({ fsTransaction, errorPointer -> Any? in
             // get accounts from the group
-            let accounts: [String: Account] = getAccounts(
+            let accounts: [String: Account] = self.getAccounts(
                 withIds: accountIds,
                 inTransaction: fsTransaction,
                 withErrorPointer: errorPointer
             )
 
-            let amount: Int = calcAmountGroupAmount(withAccounts: Array(accounts.values))
-//            let accounts: [String: Account] = Dictionary(uniqueKeysWithValues:
-//                accountIds.map {
-//                    (id: $0, (self.getAccount(withId: $0, for: fsTransaction, with: errorPointer))!)
-//                })
-//            let amount: Int = accounts.values.filter {
-//                $0.type == AccountType.asset
-//            }.map {
-//                    $0.amount ?? 0
-//            }.reduce(0, +) - accounts.values.filter {
-//                    $0.type == AccountType.liability
-//            }.map { $0.amount ?? 0 }.reduce(0, +)
+            let amount: Int = self.calcAmountGroupAmount(withAccounts: Array(accounts.values))
             // write to each account information about its membership in group
             accountIds.forEach { id in
                 var newAccountGroup = [newRef.documentID: name]
