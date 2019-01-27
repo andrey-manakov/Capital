360213910ff7691b9df4d71440eaac907caa6b01
Andrey Manakov <manakov@gmail.com>
2019-01-27 21:14:44 +0300

    NumberField Update

--- Capital/Common/Views/Label/NumberLabel.swift
+++ Capital/Common/Views/Label/NumberLabel.swift
@@ -1,9 +1,14 @@
+/// Number Label
 internal final class NumberLabel: SimpleLabel {
+	// MARK: - Initializers
+	/// Inititializes label with String
+	///
+	/// Parameter _: text to show in label
     override internal init(_ text: String? = nil) {
         super.init(text)
         textAlignment = .right
     }
-
+    /// Returns nil and implented since it is required
     internal required init?(coder aDecoder: NSCoder) {
         return nil
     }
