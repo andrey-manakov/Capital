6918c4e0c9afac4179fab7f3f62e94dfdd1892c0
Andrey Manakov <manakov@gmail.com>
2019-01-27 17:28:03 +0300

    SimpleSectionHeader Update docs

--- Capital/Common/Views/Cell/SimpleSectionHeader.swift
+++ Capital/Common/Views/Cell/SimpleSectionHeader.swift
@@ -1,20 +1,25 @@
 import UIKit
-
+/// Protocol to access `SimpleSectionHeader`
 internal protocol SimpleSectionHeaderProtocol {
+    /// Title text label inside the cell to the left
     var title: UILabel { get }
+    /// Description text label inside the cell to the right
     var desc: UILabel { get }
 }
-
+/// Section header cell for sections in UITableView
 internal class SimpleSectionHeader: UITableViewHeaderFooterView, SimpleSectionHeaderProtocol {
+    // MARK: - Properties
+    /// Title text label inside the cell to the left
     internal var title = UILabel()
+    /// Description text label inside the cell to the right
     internal var desc = UILabel()
-
+    // MARK: - INitializers
     override internal init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         contentView.add(view: title, withConstraints: ["H:|-15-[v]-65-|", "V:|-7-[v(30)]"])
         contentView.add(view: desc, withConstraints: ["H:[v(50)]-15-|", "V:|-7-[v(30)]"])
     }
-
+    /// Returns nil and implented since it is required
     internal required init?(coder aDecoder: NSCoder) {
         return nil
     }
