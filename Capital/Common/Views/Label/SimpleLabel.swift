4b746ff6531f9bcac9dbc213607cf4f1b3042fe8
Andrey Manakov <manakov@gmail.com>
2019-01-28 11:08:53 +0300

    Update SimpleLabel.swift

--- Capital/Common/Views/Label/SimpleLabel.swift
+++ Capital/Common/Views/Label/SimpleLabel.swift
@@ -1,8 +1,14 @@
+/// Protocol to access `SimpleLabel`
 internal protocol SimpleLabelProtocol {
     var text: String? { get set }
 }
 
+/// Subclass of UILabel with basic settings, to be used as a base class through out the app
 internal class SimpleLabel: UILabel, SimpleLabelProtocol {
+    // MARK: - Initializers
+    /// Initializes with text to be shown
+    ///
+    /// - Parameter text: text to be shown in label
     internal init(_ text: String? = nil) {
         super.init(frame: CGRect.zero)
         self.text = text
@@ -10,6 +16,12 @@ internal class SimpleLabel: UILabel, SimpleLabelProtocol {
         numberOfLines = 2
     }
 
+    /// Initializer with additional formatting options
+    ///
+    /// - Parameters:
+    ///   - text: text to be shown
+    ///   - alignment: text alignment
+    ///   - lines: number of lines in label
     internal convenience init(_ text: String? = nil, alignment: NSTextAlignment? = nil, lines: Int? = nil) {
         self.init(text)
         if let alignment = alignment {
@@ -20,6 +32,9 @@ internal class SimpleLabel: UILabel, SimpleLabelProtocol {
         }
     }
 
+    /// Returns nil and implented since it is required
+    ///
+    /// - Parameter aDecoder: coder token to decode data
     internal required init?(coder aDecoder: NSCoder) {
         return nil
     }
